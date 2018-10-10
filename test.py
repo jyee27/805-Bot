27b711acc96df31d2449238460e709d6940af482
John Yee <jyee27@calpoly.edu>
2018-10-10 00:06:40 -0700

    added scheduled message

--- test.py
+++ test.py
@@ -2,20 +2,22 @@
 import discord
 import requests
 import re
+import datetime
+import asyncio
 from bs4 import BeautifulSoup
 
 url = "https://menus.calpolycorporation.org/805kitchen/"
 # hdr = {'User-Agent':'Mozilla/5.0',
 #        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
-r = requests.get(url)
-soup = BeautifulSoup(r.text, 'html.parser')
+# r = requests.get(url)
+# soup = BeautifulSoup(r.text, 'html.parser')
 # for s in soup.find_all('p'):
 #     print(s.get_text())
 # s = ''
 # for item in soup.find_all('p'):
 #     print(item.format())
 
-TOKEN = 'XXXXXX'
+TOKEN = 'insert token here'
 
 client = discord.Client()
 
@@ -26,6 +28,8 @@ async def on_message(message):
         return
 
     if message.content.startswith('!805'):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
         msg = ''
         for s in soup.find_all(['h2', 'h4', 'p']):
             premsg = s.get_text()
@@ -45,6 +49,32 @@ async def on_message(message):
         # msg = re.sub('\n+', '\n', msg)
         await client.send_message(message.channel, msg)
 
+async def scheduled_message():
+    await client.wait_until_ready()
+    channel = discord.Object(id='insert channel id here')
+    while not client.is_closed:
+        t = datetime.datetime.now()
+        if t.hour == 8 and t.minute == 5:
+            r = requests.get(url)
+            soup = BeautifulSoup(r.text, 'html.parser')
+            msg = ''
+            for s in soup.find_all(['h2', 'h4', 'p']):
+                premsg = s.get_text()
+                premsg = premsg.replace('\t', '')
+                premsg = re.sub('\n+', '', premsg)
+                premsg = premsg.strip()
+                if s.name == 'h2':
+                    premsg = '\n**' + premsg + '**'
+                elif s.name == 'h4':
+                    premsg = '\n\t*' + premsg + '*'
+                elif s.name == 'p':
+                    premsg = '\t\t' + premsg
+                premsg += '\n'
+                msg += premsg
+            await client.send_message(channel, msg)
+        await asyncio.sleep(60)
+
+
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -52,4 +82,5 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+client.loop.create_task(scheduled_message())
 client.run(TOKEN)
